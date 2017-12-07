6dabde3eedb258a9b2a876ff325cb23b09e66566
Antoine Pingault <antoine.pingault@gmail.com>
2017-12-07 17:46:26 +0100

    Add missing configFIle...

---
+++ config_dqm.py
@@ -0,0 +1,7 @@
+# dataDir = "/home/webdcs/webdcs/HVSCAN/%s/" % runid
+dataDir = "/eos/user/a/apingaul/RPCRND/Data/Digitiser/%s/"
+dataFileFormat = 'wave%d.txt'
+runList = [0]
+nChannels = 4
+headerSize = 7
+recordLength = 1024
